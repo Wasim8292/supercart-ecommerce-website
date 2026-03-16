4872e8480be71ac9bddc870beef02ea1854de7a3
akram <mdwasimakram0602@gmail.com>
2026-03-17 00:32:16 +0530

    fix item component case

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./RelatedProducts.css"
-import Item from '../Components/Item/item'
+import Item from "../Components/Item/Item";
 import data_product from '../../assets/data.js'
 const RelatedProducts = () => {
   return (
